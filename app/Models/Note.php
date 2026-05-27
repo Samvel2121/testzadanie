c1b1464244d812e09bfdb3e4c8ea179119b91e83
Sam <sam@example.com>
2026-05-27 19:05:48 +0300

    Configure Note model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Note extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'color',
+        'is_pinned',
+    ];
+
+    protected $casts = [
+        'is_pinned' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
